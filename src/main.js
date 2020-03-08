0e47f1a1e207f7472c3092caca4b77727c1d51e1
unix <unix.bio@gmail.com>
2020-03-09 00:50:36 +0800

    chore: add zeit-ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import ZeitUI from '@zeit-ui/vue'
 import App from './App.vue'
+import '@zeit-ui/vue/dist/zeit-ui.css'
 
 Vue.config.productionTip = false
+Vue.use(ZeitUI)
 
 new Vue({
   render: h => h(App),
